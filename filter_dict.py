5edea93381f0210b0acd0c722fb014e8f4331752
Julian Kimmig <julian.kimmig@gmx.net>
2019-07-29 13:34:54 +0200

    save

--- filter_dict.py
+++ filter_dict.py
@@ -1,21 +1,44 @@
 import inspect
 
 
-def filter_dict(dict_to_filter, target_function,check_for_kwargs=True):
-    sig = inspect.signature(target_function)
-    if check_for_kwargs:
-        if len([param.name for param in sig.parameters.values() if param.kind == param.VAR_KEYWORD])>0:
-            return dict_to_filter
-    filter_keys = [param.name for param in sig.parameters.values() if param.kind == param.POSITIONAL_OR_KEYWORD]
-    filtered_dict = {filter_key: dict_to_filter[filter_key] for filter_key in filter_keys if filter_key in dict_to_filter}
-    return filtered_dict
+def filter_dict(dict_to_filter,keywords,default=None):
+    new_dict = {key:dict_to_filter.get(key,default) for key in keywords}
+    return new_dict
+
+
+def get_method_keywords(method,return_contains_args_kwargs=True):
+    sig = inspect.signature(method)
+    filter_keys = [param.name for param in sig.parameters.values() if param.kind == param.POSITIONAL_OR_KEYWORD or param.kind == param.KEYWORD_ONLY]
+    if not return_contains_args_kwargs:
+        return filter_keys
+    return filter_keys, len([param.name for param in sig.parameters.values() if param.kind == param.VAR_POSITIONAL])>0, len([param.name for param in sig.parameters.values() if param.kind == param.VAR_KEYWORD])>0
+
+
+def call_method(target, args=None, kwargs=None):
+    if kwargs is None: kwargs = {}
+    if args is None: args = []
+
+    target_keywords, with_args, with_kwargs = get_method_keywords(target)
+    if with_kwargs:
+        target_dict = dict.fromkeys(target_keywords,None)
+        target_dict.update(kwargs)
+    else:
+        target_dict = filter_dict(dict_to_filter = kwargs, keywords = target_keywords)
+
+    return target(**target_dict)
 
 if __name__ == "__main__":
     def f(a,b=None,**kwargs):
-        pass
+        print(a,b,kwargs)
 
-    print(filter_dict({
+    kwargs = {
         'a':0,
         'b':1,
         'c':2
-    },f))
+    }
+
+    target_keywords,with_args,with_kwargs = get_method_keywords(f)
+
+    print(target_keywords,with_args,with_kwargs)
+    print(filter_dict(dict_to_filter = kwargs, keywords = target_keywords))
+    call_method(f,**kwargs)
